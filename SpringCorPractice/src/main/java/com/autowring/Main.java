ec3401e675efed2bfd2e86c98d676f84511eb2d5
Ankur951 <kumarankursingh999@gmail.com>
2024-06-19 14:22:31 +0530

    Autowring using xml

---
+++ SpringCorPractice/src/main/java/com/autowring/Main.java
@@ -0,0 +1,15 @@
+package com.autowring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/autowring/autowringconfe.xml");
+		Emp emp1= context.getBean("emp1",Emp.class);
+		System.out.println(emp1);
+	}
+
+}
